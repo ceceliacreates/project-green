3b977beb5f167741ede31a41f8848d3b99a3272d
ceceliacreates <40367173+ceceliacreates@users.noreply.github.com>
2019-05-06 10:25:33 -0400

    cleans up formatting

--- assets/coreJavaScript/app.js
+++ assets/coreJavaScript/app.js
@@ -64,10 +64,10 @@ function renderResults(
        <div class="row">
            <div class="col-lg-12 mx-auto">
            <h3 class="text-secondary text-uppercase mb-0">${trailName} (${trailLength} Mile Hike)</h3>
-               <hr class="star-dark mb-4">
+               <hr class="star-dark mb-5">
                <div class="row">
                    <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
-                       <img class="img-fluid mb-3" src="${thumbnail}" alt="">
+                       <img class="img-fluid mb-4" src="${thumbnail}" alt="">
                    </div>
                    <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
                        <h4>${location} (${travelDist} miles away)</h4>
@@ -171,34 +171,24 @@ $("#run-search").click(function(event) {
             let maximumTemp = Math.round(weather.main.temp_max);
             let conditions = weather.weather[0].description;
             let currentWeatherIcon = weather.weather[0].icon;
-            $("#cards").append(`
-                <div class="portfolio-modal mfp-hide" id="portfolio-modal-1">
-             <div class="portfolio-modal-dialog bg-white">
-              
-            
-                     <div class="row">
-                         <div class="col-lg-12 mx-auto">
-                         <h3 class="text-secondary text-uppercase mb-0">${trailName} - ${trailLength} Mile Hike - <i class='fas fa-hiking' style='font-size:48px;color:green'></i></h3>
-                             <hr class="star-dark mb-4">
-                             <div class="row">
-                                 <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
-                                     <img class="img-fluid mb-3" src="${thumbnail}" alt="">
-                                 </div>
-                                 <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
-                                     <h4>${location} - ${travelDist} miles away - <i class='fas fa-car-side' style='font-size:30px;color:green'></i></h4>
-                                     <p class="mb-1">${description}</p>
-                                     <h5 class="text-capitalize my-0">${conditions}  &raquo;<image class="my-0" src="http://openweathermap.org/img/w/${currentWeatherIcon}.png" width="80px" height="80px"></h5>
-                                     <p class="font-weight-bold align-baseline">Feels Like  &#10146;  ${feelsLike} - ºF</p>
-                                     <p class="font-weight-bold align-baseline">Highest  &#10146;  ${maximumTemp} - ºF</p>
-                                     <p class="font-weight-bold align-baseline">Lowest  &#10146;  ${minimumTemp} - ºF</p>
-                                     
-                                     <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss mr-3 text-right" href="${trailUrl}" target="_blank">
-                                     <i class="fa fa-cog fa-spin" style="font-size:20px"></i>  Details</a>
-                                         <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss text-right" href="https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=${trailLat},${trailLong}" target="_blank">
-                                         <i class='fas fa-map-marked-alt'></i>  Directions</a>
-                                 </div>
-                             </div>
-                         </div> `);
+            renderResults(
+              trailName,
+              trailLength,
+              thumbnail,
+              location,
+              travelDist,
+              description,
+              conditions,
+              currentWeatherIcon,
+              feelsLike,
+              maximumTemp,
+              minimumTemp,
+              trailUrl,
+              latitude,
+              longitude,
+              trailLat,
+              trailLong
+            );
           });
         });
       });
@@ -256,36 +246,26 @@ $("#run-search").click(function(event) {
           let feelsLike = Math.round(weather.main.temp);
           let minimumTemp = Math.round(weather.main.temp_min);
           let maximumTemp = Math.round(weather.main.temp_max);
-            let conditions = weather.weather[0].description;
-            let currentWeatherIcon = weather.weather[0].icon;
-          $("#cards").append(`
-          <div class="portfolio-modal mfp-hide" id="portfolio-modal-1">
-       <div class="portfolio-modal-dialog bg-white">
-        
-      
-               <div class="row">
-                   <div class="col-lg-12 mx-auto">
-                   <h3 class="text-secondary text-uppercase mb-0">${trailName} - ${trailLength} Mile Hike - <i class='fas fa-hiking' style='font-size:48px;color:green'></i></h3>
-                   <hr class="star-dark mb-4">
-                       <div class="row">
-                           <div class="col-lg-5 col-md-12 col-sm-12 col-xs-12">
-                               <img class="img-fluid mb-3" src="${thumbnail}" alt="">
-                           </div>
-                           <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12">
-                           <h4>${location} - ${travelDist} miles away - <i class='fas fa-car-side' style='font-size:30px;color:green'></i></h4>
-                           <p class="mb-1">${description}</p>
-                               <h5 class="text-capitalize my-0">${conditions}  &raquo;<image class="my-0" src="http://openweathermap.org/img/w/${currentWeatherIcon}.png" width="80px" height="80px"></h5>
-                               <p class="font-weight-bold align-baseline">Feels Like  &#10146;  ${feelsLike} - ºF</p>
-                               <p class="font-weight-bold align-baseline">Highest  &#10146;  ${maximumTemp} - ºF</p>
-                               <p class="font-weight-bold align-baseline">Lowest  &#10146;  ${minimumTemp} - ºF</p>
-                               
-                               <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss mr-3 text-right" href="${trailUrl}" target="_blank">
-                                     <i class="fa fa-cog fa-spin" style="font-size:20px"></i>  Details</a>
-                                         <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss text-right" href="https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=${trailLat},${trailLong}" target="_blank">
-                                         <i class='fas fa-map-marked-alt'></i>  Directions</a>
-                           </div>
-                       </div>
-                   </div> `);
+          let conditions = weather.weather[0].description;
+          let currentWeatherIcon = weather.weather[0].icon;
+          renderResults(
+            trailName,
+            trailLength,
+            thumbnail,
+            location,
+            travelDist,
+            description,
+            conditions,
+            currentWeatherIcon,
+            feelsLike,
+            maximumTemp,
+            minimumTemp,
+            trailUrl,
+            latitude,
+            longitude,
+            trailLat,
+            trailLong
+          );
         });
       });
     });
